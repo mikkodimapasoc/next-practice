65766f591bea0bc9aeb1aee2a2021a7d24069609
Mikko Dimapasoc <mikko.dimapasoc@gmail.com>
2020-12-23 13:34:51 +0800

    initial commit

---
+++ utils/auth.js
@@ -0,0 +1,92 @@
+import Router from "next/router";
+import React, { useState, useEffect, useContext, createContext } from "react";
+import firebase from "./firebase";
+
+const authContext = createContext();
+
+export function AuthProvider({ children }) {
+  const auth = useProvideAuth();
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+}
+
+export const useAuth = () => {
+  return useContext(authContext);
+};
+
+function useProvideAuth() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const handleUser = (rawUser) => {
+    if (rawUser) {
+      const user = formatUser(rawUser);
+
+      setLoading(false);
+      setUser(user);
+      return user;
+    } else {
+      setLoading(false);
+      setUser(false);
+      return false;
+    }
+  };
+
+  const signinWithGithub = () => {
+    setLoading(true);
+    return firebase
+      .auth()
+      .signInWithPopup(new firebase.auth.GithubAuthProvider())
+      .then((response) => handleUser(response.user));
+  };
+
+  const signinWithGoogle = (redirect) => {
+    setLoading(true);
+    return firebase
+      .auth()
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then((response) => {
+        handleUser(response.user);
+        if (redirect) {
+          Router.push(redirect);
+        }
+      });
+  };
+  const signinWithFacebook = () => {
+    setLoading(true);
+    return firebase
+      .auth()
+      .signInWithRedirect(new firebase.auth.FacebookAuthProvider())
+      .then((response) => handleUser(response.user))
+      .catch((err) => console.log(err));
+  };
+
+  const signout = () => {
+    return firebase
+      .auth()
+      .signOut()
+      .then(() => handleUser(false));
+  };
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged(handleUser);
+    return () => unsubscribe();
+  }, []);
+  return {
+    user,
+    loading,
+    signinWithGithub,
+    signinWithFacebook,
+    signinWithGoogle,
+    signout,
+  };
+}
+
+const formatUser = (user) => {
+  return {
+    uid: user.uid,
+    email: user.email,
+    name: user.displayName,
+    provider: user.providerData[0].providerId,
+    photoUrl: user.photoUrl,
+  };
+};
